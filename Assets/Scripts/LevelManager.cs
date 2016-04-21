5bef9b374bbc8fda14f8aea18f1809938e8176fd
Serge Kolesnikov <serge.s.kolesnikov@gmail.com>
2016-04-21 16:39:24 +0300

    initial commit

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		//Debug.Log ("New level load: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void Quit() {
+		//Debug.Log ("Quit");
+		Application.Quit ();
+	}
+
+}
---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class NumberWizard : MonoBehaviour {
+
+	public Text GuessText;
+
+	private int max;
+	private int min;
+	private int guess;
+
+	// Use this for initialization
+	void Start () {
+		StartGame ();
+	}
+
+	void StartGame() {
+
+		max = 1000;
+		min = 0;
+		NextGuess();
+	}
+
+	public void GuessHigher() {
+		min = guess;
+		NextGuess();
+	}
+
+	public void GuessLower() {
+		max = guess;
+		NextGuess();
+	}
+
+	void NextGuess() {
+		guess = Random.Range (min, max);
+		GuessText.text = guess.ToString ();
+	}
+}
